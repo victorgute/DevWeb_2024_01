b57ae59ac9fc21d4bafced5ae30a2fd4867d4b5d
Victorgute <victor.h.gutierrez.carvalho@gmail.com>
2024-04-30 21:41:00 -0300

    java_02

---
+++ JS/02_Console_Váriaveis_Operadoras.html/Atividade/main.js
@@ -0,0 +1,6 @@
+const nome = 'Victor Hugo Gutierrez Carvalho';
+const ideade = '17'
+const peso = '64'
+const altura = '1.81'
+const IMC = peso/(altura*altura)
+console.log('Meu nome é ' + nome + ' possuo altura ' + altura + ' e peso ' + peso + ' meu IMC atual é de ' + peso/altura**2 + '.')
