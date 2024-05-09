a4f329bc3b4bf051dc6ae985f213f01c2dd76cdd
Victorgute <victor.h.gutierrez.carvalho@gmail.com>
2024-05-08 21:51:18 -0300

    tcccc

--- JS/06_Laço_De_Repetição-2/main.js
+++ JS/06_Laço_De_Repetição-2/main.js
@@ -24,3 +24,15 @@ switch(cor){
     break;
     default:console.log('A cor não é vermelha e nem azul')
 }
+
+
+console.clear();
+const matrix=[
+    ['Banana','Maça','Pêra'],
+    ['Laranja',true,1],
+    [null,'Uva',-350]
+];
+//Acessando elementos diversos dos array:
+
+console.log(`acessa a primeira linha:${matrix[0]}`);
+console.log(`acessa a primeira linha:${matrix[0][1]}`);
