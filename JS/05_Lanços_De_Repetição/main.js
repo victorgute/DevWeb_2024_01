c0afa5742370ff24626d35ab26c1ed99571de056
Victorgute <victor.h.gutierrez.carvalho@gmail.com>
2024-05-08 21:01:24 -0300

    ainda no tcc

---
+++ JS/05_Lanços_De_Repetição/main.js
@@ -0,0 +1,35 @@
+// while
+let contador = 1;
+while (contador <= 10) {
+  console.log(`Valor = ${contador}`);
+  contador++;
+}
+
+// while com decremento
+console.clear();
+let cont = 10;
+while (cont >= 0) {
+  console.log(`Valor = ${cont}`);
+  cont--;
+}
+
+ 
+
+// do-while
+console.clear();
+let i = 10,
+text = '';
+do {
+text += `O número é ${i}\n`;
+i++;
+} while (i < 10);
+console.log(text);
+
+// For
+console.clear();
+for (let i = 0; i < 10; i++) console.log(`Laço For número: ${i}`);
+
+// Laços de arrays
+const frutas = ['apples', 'oranges', 'pears', 10, true];
+console.clear();
+for (let j = 0; j < frutas.length; j++) console.log(`Nome: ${frutas[j]} \n`);
