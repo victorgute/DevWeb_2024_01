fa8c135f2b75130adf4b1f11d7316265de9ff212
Leonardo <matheusleonardo224@gmail.com>
2024-05-16 21:38:01 -0300

    victor

---
+++ JS/12_Dominando_Doom/Lição/main.js
@@ -0,0 +1,11 @@
+document.title = 2024;
+
+let body = document.createElement('section',);
+let body1 = document.createElement('article');
+let body2 = document.createElement('H1');
+let body3 = document.createElement('P');
+
+body.innerHTML = 'Olá alunos';
+body1.innerHTML = 'tudo bem?'
+document.body.appendChild(body);
+heading.style.borderBottom = 'solid 3px #000';
